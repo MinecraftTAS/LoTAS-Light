6a8666b6889a1becc7865c715f0adb6197c4dda2
Scribble <scribble@minecrafttas.com>
2024-09-04 22:29:22 +0200

    Added config files

--- src/main/java/com/minecrafttas/lotas_light/LoTASLight.java
+++ src/main/java/com/minecrafttas/lotas_light/LoTASLight.java
@@ -1,8 +1,13 @@
 package com.minecrafttas.lotas_light;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import net.fabricmc.api.ModInitializer;
 
 public class LoTASLight implements ModInitializer {
+
+	public static Logger LOGGER = LogManager.getLogger("LoTAS-Light");
 
 	@Override
 	public void onInitialize() {

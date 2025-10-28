391a43df7a2d76b5b19469086591cf41398df59e
Scribble <scribble@minecrafttas.com>
2025-10-28 21:17:21 +0100

    Fix small lag when changing tickrate

--- src/main/java/com/minecrafttas/lotas_light/mixin/MixinServerTickRateManager.java
+++ src/main/java/com/minecrafttas/lotas_light/mixin/MixinServerTickRateManager.java
@@ -7,7 +7,11 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 import com.minecrafttas.lotas_light.duck.Tickratechanger;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.protocol.Packet;
 import net.minecraft.server.ServerTickRateManager;
+import net.minecraft.server.players.PlayerList;
+import net.minecraft.world.TickRateManager;
 
 @Mixin(ServerTickRateManager.class)
 public abstract class MixinServerTickRateManager {
@@ -18,6 +22,12 @@ public abstract class MixinServerTickRateManager {
 		if (!tickratechanger.isAdvanceTick()) {
 			this.updateStateToClients();
 		}
+	}
+
+	@Redirect(method = "updateStateToClients", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/players/PlayerList;broadcastAll(Lnet/minecraft/network/protocol/Packet;)V"))
+	public void redirect_updateStateToClients(PlayerList playerList, Packet<?> packet) {
+		TickRateManager tickratechanger = Minecraft.getInstance().level.tickRateManager();
+		tickratechanger.setTickRate(((ServerTickRateManager) (Object) this).tickrate());
 	}
 
 	@Shadow
